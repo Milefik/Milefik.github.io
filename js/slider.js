c163bc38e8f63b9b82a3184c05546aaf8316daee
Ekaterina Baranova <katerina.25091999@gmail.com>
2024-07-03 02:46:43 +0500

    Slider rework

--- js/slider.js
+++ js/slider.js
@@ -3,62 +3,70 @@ const cardsCount = document.getElementsByClassName('section-slider__item').lengt
 
 let cardsForPerView;
 let cardWidth;
-
-const сheckAdaptive = () => {
-	if (window.innerWidth >= 992 && window.innerWidth <= 1199) {
-		cardsForPerView = 2;
-		cardWidth = 485;
-	} else if (window.innerWidth >= 768 && window.innerWidth <= 991) {
-		cardsForPerView = 2;
-		cardWidth = 375;
-	} else if (window.innerWidth >= 576 && window.innerWidth <= 767) {
-		cardsForPerView = 2;
-		cardWidth = 288;
-	} else if (window.innerWidth <= 575) {
-		cardsForPerView = 1;
-		cardWidth = 320;
-	} else {
-		cardsForPerView = 3;
-		cardWidth = 390;
-	}
-}
-
-сheckAdaptive();
-
 let currentIndex = 0;
-let currentOffset = 0;
+
+const resizeWindow = () => {
+    currentIndex = 0;
+    slider.style.left = '0';
+
+    if (window.innerWidth >= 992 && window.innerWidth <= 1199) {
+        cardsForPerView = 2;
+        cardWidth = 485;
+
+        return;
+    }
+
+    if (window.innerWidth >= 768 && window.innerWidth <= 991) {
+        cardsForPerView = 2;
+        cardWidth = 375;
+
+        return;
+    }
+
+    if (window.innerWidth >= 576 && window.innerWidth <= 767) {
+        cardsForPerView = 2;
+        cardWidth = 288;
+
+        return;
+    }
+
+    if (window.innerWidth <= 575) {
+        cardsForPerView = 1;
+        cardWidth = 320;
+
+        return;
+    }
+
+    cardsForPerView = 3;
+    cardWidth = 390;
+};
 
 const moveToNext = () => {
-    if (currentOffset === cardsCount - cardsForPerView) {
-			slider.style.left = '0px';
-			currentIndex = 0;
-			currentOffset = 0;
+    if (currentIndex === cardsCount - cardsForPerView) {
+        slider.style.left = '0px';
+        currentIndex = 0;
 
-			return;
+        return;
     }
 
     currentIndex += 1;
-    currentOffset += 1;
-		
     slider.style.left = `-${cardWidth * currentIndex}px`;
-	};
+};
 
 const moveToPrev = () => {
-    if (!currentOffset) return;
+    if (!currentIndex) {
+        slider.style.left = `-${cardWidth * (cardsCount - cardsForPerView)}px`;
+        currentIndex = cardsCount - cardsForPerView;
+
+        return;
+    }
 
     currentIndex -= 1;
-    currentOffset -= 1;
-
     slider.style.left = `-${cardWidth * currentIndex}px`;
-}
+};
 
-const reset = () => {
-	slider.style.left = '0px';
-	currentIndex = 0;
-	currentOffset = 0;
-}
+window.addEventListener('load', resizeWindow);
+window.addEventListener('resize', resizeWindow);
 
 document.querySelector('.section-slider__arrow--right').addEventListener('click', moveToNext);
 document.querySelector('.section-slider__arrow--left').addEventListener('click', moveToPrev);
-window.addEventListener('resize', сheckAdaptive);
-window.addEventListener('resize', reset);
